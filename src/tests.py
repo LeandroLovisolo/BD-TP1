725bc192f682c530e0c3a06f6663aca12dc994cd
Leandro Lovisolo <leandro@leandro.me>
2014-10-03 14:57:45 -0300

    Tests para empadronar_alumno

--- src/tests.py
+++ src/tests.py
@@ -1,10 +1,30 @@
-from tp_api import model_test as mt
+#!/usr/bin/env python2
+
 import unittest
+import tp_api as api
 
 class TestModel(unittest.TestCase):
-	def setUp(self):
-		mt.empadronar_alumno("")
+    def setUp(self):
+        # Crear una base en memoria y crear las tablas
+        self.connector = api.bd_connector()
+        self.connector.connect(bd=':memory:')
+        f = open('../db/facultad.sql', 'r')
+        self.connector.conn.executescript(f.read())
+
+        # El modelo ahora usa la base en memoria en lugar del archivo facultad.db
+        self.model = api.model_test(self.connector)
+
+    def test_empadronar_alumno(self):
+        dni = 1234
+        nombre = 'Alumno'
+
+        self.model.empadronar_alumno(dni, nombre)
+        with self.connector as c:
+            c.execute('SELECT dni, tipo FROM empadronado WHERE dni = ?', (dni,))
+            row = c.fetchone()
+            self.assertIsNotNone(row)
+            self.assertEquals(row[1], api.TIPO_ESTUDIANTE)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
--- src/tp_api.py
+++ src/tp_api.py
@@ -10,22 +10,37 @@ import sqlite3
 # se da un ejemplo incompleto con el motor SQLite, pueden  adaptarlo
 # a cualquiera de los motores permitidos
 
-ID_FACULTAD_DEFAULT = 1
 NACIMIENTO_DEFAULT = '01/01/2000'
 TODAY = time.strftime("%d/%m/%Y")
-TIPO_GRADUADO_DEFAULT = 0
-TIPO_PRIFESOR_DEFAULT = 0
+
+ID_FACULTAD_DEFAULT = 1
+
+# Valores de la columna 'tipo' de la tabla 'empadronado'
+TIPO_ESTUDIANTE = 0
+TIPO_GRADUADO   = 1
+TIPO_PROFESOR   = 2
 
 class bd_connector():
-	# Funcion que crea la conexion con su BD
-	def connect(self, port,username,password,bd="bd",host="localhost"):
-		self.conn = sqlite3.connect(bd)
-	
-	# Funcion que ejecuta queries sin esperar resultado y las comitea
-	def query_without_result(self,aQuery):
-		c = self.conn.cursor()
-		c.execute(aQuery)
-		self.conn.commit()
+    # Funcion que crea la conexion con su BD
+    def connect(self, port='', username='', password='', bd='bd', host='localhost'):
+        self.conn = sqlite3.connect(bd)
+    
+    # Funcion que ejecuta queries sin esperar resultado y las comitea
+    def query_without_result(self, aQuery):
+        c = self.conn.cursor()
+        c.execute(aQuery)
+        self.conn.commit()
+
+    def __enter__(self):
+        self.cur = self.conn.cursor()
+        return self.cur
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is None and exc_value is None and traceback is None:
+            self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
 
 # Clase para testear una subparte del modelo realizado. La subparte a
 # testear corresponde a lo referido en una sola facultad. Es por eso
@@ -33,60 +48,58 @@ class bd_connector():
 
 class model_test():
 
-	def connect(self):
-		conector = bd_connector()
-		conector.connect('fruta', 'fruta', 'fruta', '../db/facultad.db')
-		return conector
+    # Permite usar un conector distinto (ej.: a una base en memoria) desde los tests
+    def __init__(self, connector=None):
+        if connector is None:
+            self.connector = bd_connector()
+            self.connector.connect('../db/facultad')
+        else:
+            self.connector = connector
 
-	def execute_query(self, query):
-		conector=self.connect()
-		conector.query_without_result(query)
+    def execute_query(self, query):
+        print query
+        self.connector.query_without_result(query)
 
-	def crear_empadronado(self, dni, nombre, tipo):
-		query_empadronado = "INSERT INTO empadronado (dni, nombre, fecha_de_nacimiento, id_facultad, tipo) VALUES (\"%s\", \"%s\", \"%s\", %s, %s)"%(dni, nombre, NACIMIENTO_DEFAULT, ID_FACULTAD_DEFAULT, str(tipo))
-		print(query_empadronado)
-		self.execute_query(query_empadronado)
-	#Tipos de empadronado: 0 = estudiante, 1 = graduado, 2 = profesor
-	# Funciones para empadronar personas
-	def empadronar_alumno(self, dni, nombre): 
-		tipo = 0
-		self.crear_empadronado(dni, nombre, tipo)
-		query_alumno = "INSERT INTO estudiante (dni, fecha_inscripcion) VALUES (\"%s\", \"%s\")" % (dni, TODAY)
-		print(query_alumno)
-		self.execute_query(query_alumno)
+    def crear_empadronado(self, dni, nombre, tipo):
+        query_empadronado = "INSERT INTO empadronado (dni, nombre, fecha_de_nacimiento, id_facultad, tipo) VALUES (\"%s\", \"%s\", \"%s\", %s, %s)"%(dni, nombre, NACIMIENTO_DEFAULT, ID_FACULTAD_DEFAULT, str(tipo))
+        self.execute_query(query_empadronado)
 
-	#tipo de graduado 0 = UBA, 1 = otras
-	def empadronar_graduado(self, dni, nombre):
-		tipo = 1
-		self.crear_empadronado(dni, nombre, tipo)
-		query_graduado = "INSERT INTO graduado (dni, tipo) VALUES (\"%s\", %s)" % (dni, TIPO_GRADUADO_DEFAULT)
-		print(query_graduado)
-		self.execute_query(query_graduado)
+    #Tipos de empadronado: 0 = estudiante, 1 = graduado, 2 = profesor
+    # Funciones para empadronar personas
+    def empadronar_alumno(self, dni, nombre): 
+        tipo = 0
+        self.crear_empadronado(dni, nombre, tipo)
+        query_alumno = "INSERT INTO estudiante (dni, fecha_inscripcion) VALUES (\"%s\", \"%s\")" % (dni, TODAY)
+        self.execute_query(query_alumno)
 
-		query_graduado_UBA = "INSERT INTO graduado_uba (dni) VALUES (\"%s\")" % (dni)
-		print(query_graduado_UBA)
-		self.execute_query(query_graduado_UBA)
+    #tipo de graduado 0 = UBA, 1 = otras
+    def empadronar_graduado(self, dni, nombre):
+        tipo = 1
+        self.crear_empadronado(dni, nombre, tipo)
+        query_graduado = "INSERT INTO graduado (dni, tipo) VALUES (\"%s\", %s)" % (dni, TIPO_GRADUADO)
+        self.execute_query(query_graduado)
 
-	#tipo de profesor 0 = regular, 1 = adjunto
-	def empadronar_profesor(self, dni,nombre):	
-		tipo = 2
-		self.crear_empadronado(dni, nombre, tipo)
-		query_graduado = "INSERT INTO profesor (dni, tipo) VALUES (\"%s\", %s)" % (dni, TIPO_GRADUADO_DEFAULT)
-		print(query_graduado)
-		self.execute_query(query_graduado)
+        query_graduado_UBA = "INSERT INTO graduado_uba (dni) VALUES (\"%s\")" % (dni)
+        self.execute_query(query_graduado_UBA)
 
-		query_graduado_regular = "INSERT INTO profesor_regular (dni) VALUES (\"%s\")" % (dni)
-		print(query_graduado_regular)
-		self.execute_query(query_graduado_regular)
-	
-	# Funcion que afilia a una persona (dni_afiliante) a una agrupacion (id_agrupacion)
-	def afiliar_a_agrupacion(self, dni_afiliante, id_agrupacion): pass
+    #tipo de profesor 0 = regular, 1 = adjunto
+    def empadronar_profesor(self, dni,nombre):  
+        tipo = 2
+        self.crear_empadronado(dni, nombre, tipo)
+        query_graduado = "INSERT INTO profesor (dni, tipo) VALUES (\"%s\", %s)" % (dni, TIPO_GRADUADO)
+        self.execute_query(query_graduado)
 
-	# Funciones para setear la cantidad de votos que obtuvo cada candidato a consejero en la votacion con fecha=fecha
-	def set_cant_votos_cantidato_a_consejero(self, dni_candidato, cantidad_de_votos, fecha): pass
-	
-	# Funcion que determina como esta compuesto el consejo directivo en la fecha=fecha
-	def composicion_consejo(self,fecha): pass
-	
-	# Funcion que emite un voto de un consejero (dni_votador) para un candidato (dni_candidato) de decano en la fecha=fecha
-	def set_voto_para_decano(self,dni_consejero_votador, dni_candidato,cantidad_de_votos,fecha): pass
+        query_graduado_regular = "INSERT INTO profesor_regular (dni) VALUES (\"%s\")" % (dni)
+        self.execute_query(query_graduado_regular)
+    
+    # Funcion que afilia a una persona (dni_afiliante) a una agrupacion (id_agrupacion)
+    def afiliar_a_agrupacion(self, dni_afiliante, id_agrupacion): pass
+
+    # Funciones para setear la cantidad de votos que obtuvo cada candidato a consejero en la votacion con fecha=fecha
+    def set_cant_votos_cantidato_a_consejero(self, dni_candidato, cantidad_de_votos, fecha): pass
+    
+    # Funcion que determina como esta compuesto el consejo directivo en la fecha=fecha
+    def composicion_consejo(self,fecha): pass
+    
+    # Funcion que emite un voto de un consejero (dni_votador) para un candidato (dni_candidato) de decano en la fecha=fecha
+    def set_voto_para_decano(self,dni_consejero_votador, dni_candidato,cantidad_de_votos,fecha): pass
